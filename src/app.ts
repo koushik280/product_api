902e2004fc529fff38c0e1441c352e6fb9175a01
koushik280 <koushikarmakar679@gmail.com>
2026-01-04 20:25:08 +0530

    Add CORS middleware for frontend access

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import cors from "cors"
+
 import productroutes from  "./routes/product.routes"
+
 const app = express();
 app.use(express.json());
-
+app.use(cors())
 app.use("/api",productroutes)
 export default app;
